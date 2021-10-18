7bfa78590f441e5aeaec5e9cd7f66cba75de6f6f
Arthur Valadao <arth.valadao@gmail.com>
2021-10-18 11:46:13 -0400

    Register screen complete

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -13,7 +13,7 @@ export default function Register() {
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#e02041" />
             Não tenho cadastro
           </Link>
@@ -25,7 +25,6 @@ export default function Register() {
           <div className="input-group">
             <input placeholder="Cidade" />
             <input placeholder="UF" style={{ width: 80 }} />
-
           </div>
           <button className="button" type="submit">Cadastrar</button>
         </form>
